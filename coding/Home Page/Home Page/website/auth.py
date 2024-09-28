74588a27d3eb0493c4ff30f7455d895b50d3fa51
Arya Vera <1231101846@student.mmy.edu.my>
2024-09-28 09:02:05 +0800

    deleted 'coding' file

--- coding/Home Page/Home Page/website/auth.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from flask_login import login_required, logout_user, current_user
-from . import db
-
-auth = Blueprint('auth', __name__)
-
-@auth.route('/logout')
-def logout():
-    return redirect(url_for('')) #login page url
